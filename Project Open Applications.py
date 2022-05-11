6a1b8bd0ff35b26ecf926e119d98367765a0677a
krisha232 <78255450+krisha232@users.noreply.github.com>
2022-05-11 18:12:39 +0530

    Update Project Open Applications.py

--- Project Open Applications.py
+++ Project Open Applications.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from PIL import ImageTk,Image
 from tkinter import filedialog
+from tkinter import messagebox
+import webbrowser
 import os
 
 root=Tk()
@@ -26,20 +28,40 @@ def Openfile():
     global name
     my_text.delete(1.0,END)    
     input_file_name.delete(0,END)
-    text_file= filedialog.askopenfilename(title="Open Text File", filetypes=(("Text Files", "*.txt"),))
+    text_file= filedialog.askopenfilename(title="Open Text File", filetypes=(("Text Files", "*.html"),))
     print(text_file)
     name= os.path.basename(text_file)
     formated_name=name.split('.')[0]
     input_file_name.insert(END,formated_name)
     root.title(formated_name)
-    text_file=open(name,'r')
+    text_file= open(name,'r')
     paragraph=text_file.read()
     my_text.insert(END,paragraph)
     text_file.close()
 
+def save():
+    input_name=input_file_name.get()
+    file=open(input_name+".html","w")
+    data=my_text.get("1.0",END)
+    print(data)
+    file.write(data)
+    input_file_name.delete(0,END)
+    my_text.delete(1.0,END)
+    messagebox.showinfo("Update","Success")
+
+def run():
+    global name
+    webbrowser.open(name)
+    
+    
 open_btn =Button(root,image=open_img,text = "Open File", command= Openfile)
 open_btn.place(relx=0.05,rely=0.03, anchor=CENTER)
 
+save_btn =Button(root,image=save_img,text = "Save File", command= save)
+save_btn.place(relx=0.11,rely=0.03, anchor=CENTER)
+
+run_btn =Button(root,text = "Run File", command= run)
+run_btn.place(relx=0.17,rely=0.03, anchor=CENTER)
 
 
 root.mainloop()
